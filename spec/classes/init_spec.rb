74e2e2f0d5ff1e079f0c3b79f66afd3d8fbad699
t0xicCode <xavier.l@t0xic.co.de>
2015-05-16 12:27:09 -0400

    Updated rspec tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,46 +2,37 @@ require 'spec_helper'
 
 describe 'solr' do
 
-  context "where params are not passed (default case)" do
+  {'Ubuntu' => 'Debian', 'Debian' => 'Debian'}.each do |system, family|
+    context "when on system #{system}" do
+      if system == 'Debian'
+        let :facts do
+          {
+            :osfamily        => family,
+            :operatingsystem => system,
+            :operatingsystemmajrelease => 7,
+          }
+        end
+      else
+        let :facts do
+          {
+            :osfamily        => family,
+            :operatingsystem => system,
+          }
+        end
+      end
 
-    it { should contain_solr__params }
+      it { should contain_class('solr') }
+      it { should contain_class('solr::install') }
+      it { should contain_class('solr::config') }
+      it { should contain_class('solr::service') }
 
-    it { should contain_class('solr::install') }
-
-    it { should contain_class('solr::config')
-        .with({
-                'cores'     => ['default'],
-                'version'   => '4.7.2',
-                'mirror'    => 'http://www.us.apache.org/dist/lucene/solr',
-              })
-    }
-
-    it { should contain_class('solr::service') }
-
+      it {
+        should contain_service('jetty')
+      }
+    end
   end
 
-  context "where params are passed" do
-
-    let(:params) { {
-        :cores    => ['dev', 'prod'],
-        :version  => '5.6.2',
-        :mirror   => 'http://some-random-site.us',
-    } }
-
-    it { should contain_solr__params }
-
-    it { should contain_class('solr::install') }
-
-    it { should contain_class('solr::config')
-        .with({
-                'cores'      => ['dev', 'prod'],
-                'version'    => '5.6.2',
-                'mirror'     => 'http://some-random-site.us',
-              })
-    }
-
-    it { should contain_class('solr::service') }
-
+  context 'when on an unknown system' do
+    it { expect { should raise_error(Puppet::Error) } }
   end
-
 end
--- spec/defines/core_spec.rb
+++ spec/defines/core_spec.rb
@@ -2,27 +2,55 @@ require 'spec_helper'
 
 describe 'solr::core', :type => :define do
   let(:title) { 'default' }
+  let(:params) { {
+    solr_home: '/usr/share/solr',
+    solr_data: '/var/lib/solr'
+  } }
 
-  it { should contain_file('/usr/share/solr/default').with({
-    'ensure'    => 'directory',
-    'owner'     => 'jetty',
-    'group'     => 'jetty',
-    'require'   => 'File[/usr/share/solr]'})
-  }
+  context 'managing conf' do
+    it { should contain_file('/usr/share/solr/default').with({
+      'ensure'    => 'directory',
+      'owner'     => 'jetty',
+      'group'     => 'jetty' })
+    }
 
-  it { should contain_file('/usr/share/solr/default/conf').with({
-    'ensure'    =>    'directory',
-    'recurse'   =>    'true',
-    'source'    =>    'puppet:///modules/solr/conf',
-    'require'   =>    'File[/usr/share/solr/default]'})
-  }
+    it { should contain_file('/usr/share/solr/default/conf').with({
+      'ensure'    =>    'directory',
+      'recurse'   =>    'true',
+      'source'    =>    'puppet:///modules/solr/conf' })
+    }
 
-  it { should contain_file('/var/lib/solr/default').with({
-    'ensure'    => 'directory',
-    'owner'     => 'jetty',
-    'group'     => 'jetty',
-    'require'   => 'File[/usr/share/solr/default/conf]' })
-  }
+    it { should contain_file('/var/lib/solr/default').with({
+      'ensure'    => 'directory',
+      'owner'     => 'jetty',
+      'group'     => 'jetty' })
+    }
+  end
 
+  context 'not managing conf' do
+    let(:params) { {
+      manage_conf: false,
+      solr_home: '/usr/share/solr',
+      solr_data: '/var/lib/solr'
+    } }
+
+    it { should contain_file('/usr/share/solr/default').with({
+      'ensure'    => 'directory',
+      'owner'     => 'jetty',
+      'group'     => 'jetty' })
+    }
+
+    it { should_not contain_file('/usr/share/solr/default/conf').with({
+      'ensure'    =>    'directory',
+      'recurse'   =>    'true',
+      'source'    =>    'puppet:///modules/solr/conf' })
+    }
+
+    it { should contain_file('/var/lib/solr/default').with({
+      'ensure'    => 'directory',
+      'owner'     => 'jetty',
+      'group'     => 'jetty' })
+    }
+  end
 end
 
